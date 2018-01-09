06a1272dd935cbf3e0453640099ce104af53a4b7
Brad Hambrice <hambrice@uab.edu>
2018-01-09 00:56:56 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,5 +9,8 @@ class Anagram
     array.collect do |option|
       new_array << option if word.split("").sort == option.split("").sort 
     end
+    new_array.delete_if do |options|
+      options == nil 
+    end
 end
 end
